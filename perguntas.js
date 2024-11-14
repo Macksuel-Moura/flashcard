591b7b05696adf9a815bcce7514d244ac9c7957d
Macksuel Moura <0000110699730xsp@aluno.educacao.sp.gov.br>
2024-11-14 15:14:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimento avançado',
+    'Quem é fera?',
+    'Trabalhadores de Carapicuíba'
+)
+
+criaCartao(
+    'Conhecimento básico',
+    'Quem não é fera?',
+    'Trabalhador de Osasco'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem é ladrão?',
+    'Torcedor do Corinthians'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual o melhor filme da industria brasileira?',
+    'Cinderella Baiana'
+)
